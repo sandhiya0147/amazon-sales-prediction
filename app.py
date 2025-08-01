e52b7674b8446943f8859504a1cf9192e94554f2
sandhiya <sandhiyasivakumar0147@gmail.com>
2025-08-01 21:26:40 +0530

    Fix

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def index():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)
+
